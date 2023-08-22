beb3eec0e4d6e0cfd544fbf9c39b297d27cecc1a
Juan <saraviamartin16@hotmail.com>
2023-08-22 11:08:12 -0300

    creando paginas con renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function Homepage(){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home</h1>
+      <p>Este es un ejemplo para crear un Router desde cero</p>
+      <a href="/about">Sobre Nosotros</a>
     </>
+  )
+}
+export function AboutPage(){
+  return (
+    <>
+      <h1>About</h1>
+      <img src="https://www.mundodeportivo.com/alfabeta/hero/2023/05/satoru-gojo-jujutsu-kaisen.jpg?width=1200s" alt="" />
+      <p>Hola, estoy creando un clon de react Router</p>
+      <a href="/">ir a la Home</a>
+    </>
+  )
+}
+
+
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname) 
+  return(
+    <main>
+      {currentPath === "/" && <Homepage/>}
+      {currentPath === "/about" && <AboutPage/>}
+    </main>
   )
 }
 
