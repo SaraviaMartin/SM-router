4e6f60b433ae47ccebe23ada862fb14158b34815
Juan <saraviamartin16@hotmail.com>
2023-08-23 16:00:01 -0300

    agregando funcionalidades con el teclado y el click derecho

--- src/Link.jsx
+++ src/Link.jsx
@@ -9,7 +9,14 @@ export function navigate(href){
   export function Link({target, to, ...props}){
     const handleClick = (event) => {
         event.preventDefault()
-        navigate(to)
+
+        const isMainEvent = event.button === 0 // primary click
+        const isModifiedEvent = event.metaKey || event.altKEy || event.ctrlKey || event.shiftKey
+        const isManageableEvent = target === undefined || target === '_self'
+
+        if(isMainEvent && isManageableEvent && !isModifiedEvent){
+          navigate(to)
+        }
     }
     return <a onClick={handleClick} href={to} target={target} {...props} />
   }
