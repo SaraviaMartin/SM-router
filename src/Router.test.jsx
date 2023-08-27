d72e6c69c70282d155bb20bd5ac1a95a3186a076
Juan <saraviamartin16@hotmail.com>
2023-08-27 12:06:14 -0300

    testing

--- src/Router.test.jsx
+++ src/Router.test.jsx
@@ -1,6 +1,8 @@
 import { cleanup, render, screen} from "@testing-library/react";
 import { describe, it, expect, beforeEach, vi } from "vitest";
-import{Router} from "./Router.jsx" 
+import { Router } from "./Router.jsx" 
+import { Route } from "./Route.jsx"
+import { Link } from "./Link.jsx"
 import { getCurrentPath } from "./utils.js";
 
 vi.mock('./utils.js', ()=> ({
@@ -23,19 +25,44 @@ describe('Router', ()=> {
         expect(screen.getByText('404')).toBeTruthy()
     })
 
-    it('should render the first component of the first route that matches'), ()=>{
-        getCurrentPath.mockReturnValue("/about")
-        const routes= [
-            {
-                path: '/',
-                component: ()=> <h1>Home</h1>
-            },
-            {
-                path: '/about',
-                component: ()=> <h1>About</h1>
-            }
+    it('should render the component of the first route that matches', () => {
+        getCurrentPath.mockReturnValue('/about')
+    
+        const routes = [
+          {
+            path: '/',
+            Component: () => <h1>Home</h1>
+          },
+          {
+            path: '/about',
+            Component: () => <h1>About</h1>
+          }
         ]
-        render(<Router routes={routes}/>)
+    
+        render(<Router routes={routes} />)
         expect(screen.getByText('About')).toBeTruthy()
-    }
+      })
+
+    it('should navigate using Links', () => {
+        getCurrentPath.mockReturnValue('/')
+
+        render (
+            <Router>
+                <Route path="/" Component={() => {
+                    return (
+                        <>
+                            <h1>Home</h1>
+                            <Link to="/about">About</Link>
+                        </>
+                    )
+                }} />
+            </Router>
+        )
+
+        //Click on the link
+        screen.getByText('About').click()
+
+        //Check that the new route is rendered
+        expect(screen.getByText('About'))
+    })
 })
